0e77ad59813275cc770e2ed08d49ca2c2369d044
Jamie Dishy <Jamie Dishy@192.168.0.16>
2019-09-17 13:43:03 -0400

    day3

--- LeetCode/src/Easy/ReverseInt.java
+++ LeetCode/src/Easy/ReverseInt.java
@@ -9,24 +9,38 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ReverseInt {
-	
+
 	static int a = 1234;
-	
+
 	public static ArrayList<Integer> reverse(int a) {
 		List<Integer> reverseList = new ArrayList<Integer>();
 		while (a / 10 > 0) {
-			reverseList.add(a%10);
+			reverseList.add(a % 10);
 			a = a / 10;
-			if (a/10 == 0) {
+			if (a / 10 == 0) {
 				reverseList.add(a);
 			}
 		}
 		return (ArrayList<Integer>) reverseList;
 	}
 
-	
 	public static void main(String[] args) {
-	 System.out.println(reverse(a));
+		System.out.println(reverse(a));
+
 	}
 
 }
+
+//class Solution {
+//    public int reverse(int x) {
+//        int rev = 0;
+//        while (x != 0) {
+//            int pop = x % 10;
+//            x /= 10;
+//            if (rev > Integer.MAX_VALUE/10 || (rev == Integer.MAX_VALUE / 10 && pop > 7)) return 0;
+//            if (rev < Integer.MIN_VALUE/10 || (rev == Integer.MIN_VALUE / 10 && pop < -8)) return 0;
+//            rev = rev * 10 + pop;
+//        }
+//        return rev;
+//    }
+//}
