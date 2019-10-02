877910dfc9275f1eeaf978edf0dc40902118329d
Jamie Dishy <Jamie Dishy@192.168.0.16>
2019-10-02 09:59:48 -0400

    Day6

--- LeetCode/src/Easy/AddBinary.java
+++ LeetCode/src/Easy/AddBinary.java
@@ -1,4 +1,6 @@
 package Easy;
+// Learned: "carry-over variable"
+// Learned: Adding character int values using Character.getNumericValue
 
 public class AddBinary {
 	static String a = "11001";
@@ -8,62 +10,47 @@ public class AddBinary {
 	public static String addBinary(String x, String y) {
 		String c = "";
 		int yIndex = y.length() - 1;
-		
-		for (int i=x.length()-1; i>=0; i--) {
+
+		for (int i = x.length() - 1; i >= 0; i--) {
 			int added = 0; // x[i] + y[yIndex]
-			
-			//System.out.println(x.charAt(i));
-			//System.out.println(co);
 			if (yIndex < 0) {
-				if (x.charAt(i)=='1' && co.equals("1")) {
+				if (x.charAt(i) == '1' && co.equals("1")) {
 					c = "0" + c;
-				//	System.out.println(c);
-					co="1";
+					co = "1";
+				} else if (x.charAt(i) == '0' && co.equals("1")) {
+					c = "1" + c;
+					co = "0";
+				} else if (co.equals("0")) {
+					c = x.charAt(i) + c;
 				}
-				else if (x.charAt(i)=='0' && co.equals("1")) {
-					c="1" + c;
-					co="0";
-				}
-				else if (co.equals("0")){
-					c=x.charAt(i) + c;
-				//	co="0";
-				}
-			}
-			else {
+			} else { // not out of bounds
 				added = Character.getNumericValue(x.charAt(i)) + Character.getNumericValue(y.charAt(yIndex));
 				yIndex--;
-			//	System.out.println(added);
 			}
-			if (added==2 && co.equals("1") ) {
-				//added = 1;
-				c = "1" + c;
-				co = "1";
-			//	yIndex--;
+			if (added == 2) {
+				if (co.equals("1")) {
+					c = "1" + c;
+					co = "1";
+				} else if (co.equals("0")) {
+					c = "0" + c;
+					co = "1";
+				}
 			}
-			if (added==2 && co.equals("0")) {
-				//added=0;
+			else if (added == 1) {
+				if (co.equals("1")) {
+					c = "0" + c;
+					co = "1";
+				} else if (co.equals("0")) {
+					c = "1" + c;
+					co = "0";
+				}
+			} else if (added == 0 && co.contentEquals("0")) {
 				c = "0" + c;
-				co = "1";
-			//	yIndex--;
-			}
-			if (added==1 && co.equals("1")) {
-				c="0"+c;
-				co="1";
-			//	yIndex--;
-			}
-			if (added==1 && co.equals("0")) {
-				c="1" + c;
-				co="0";
-				//yIndex--;
-			}
-			else if (added == 0 && co.contentEquals("0")){
-					c="0"+c;
-					co="0";
-					//System.out.println(c);
+				co = "0";
 			}
 		}
 		if (co.equals("1")) {
-			c="1" + c;
+			c = "1" + c;
 		}
 		System.out.println(c);
 		return c;
@@ -71,7 +58,5 @@ public class AddBinary {
 
 	public static void main(String[] args) {
 		addBinary(a, b);
-
 	}
-
 }
