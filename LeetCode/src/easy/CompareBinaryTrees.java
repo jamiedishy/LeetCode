5572e69d2c458bda12f7f990a9f5f16efd4f1dcc
Jamie Dishy <Jamie Dishy@192.168.0.16>
2019-10-02 11:12:00 -0400

    Day 7

---
+++ LeetCode/src/easy/CompareBinaryTrees.java
@@ -0,0 +1,38 @@
+package easy;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+import javax.swing.tree.TreeNode;
+
+public class CompareBinaryTrees {
+	
+	// static int[] treeA = { 1, 2, 1 };
+    // static int[] treeB = { 1, 2, 1 };
+
+//	public static boolean treeCompare(int[] a, int[] b) {
+//		if (a.length != b.length) {
+//			System.out.println(false);
+//		}
+//
+//		int i = 0; // index value of a
+//		int j = 0; // index value of b
+//
+//		while (i < a.length && j < b.length) {
+//			if (a[i] == b[j]) {
+//				i++;
+//				j++;
+//				// System.out.println(true);
+//			} else {
+//				System.out.println(false);
+//				return false;
+//			}
+//		}
+//		return true;
+//	}
+
+	public static void main(String[] args) {
+		// treeCompare(treeA, treeB);
+	}
+
+}
