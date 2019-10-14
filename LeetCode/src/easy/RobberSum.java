9afded4a3cdf09989ff59ae9194998a43d1353c4
Jamie Dishy <Jamie Dishy@MSI>
2019-10-13 20:56:37 -0400

    Day 9

---
+++ LeetCode/src/easy/RobberSum.java
@@ -0,0 +1,34 @@
+package easy;
+
+public class RobberSum {
+	
+	static int[] cashPerHouse = {3, 7, 1, 2, 4};
+	
+	public static int cashSum (int[] A) {
+		
+		int sumEven = 0;
+		int sumOdd = 0;
+		
+		if (A.length % 2 == 0) {
+			int j = 0;
+			for (int i = 1; i < A.length; i=i+2) {
+				sumOdd += A[i];
+				sumEven += A[j];
+				j += 2;
+			}
+		}
+		else {
+			int i = 0;
+			for (int j = 1; j < A.length; j=j+2) {
+				sumEven += A[j];
+				sumOdd += A[i];
+				i += 2;
+			}
+		}
+		return Math.max(sumEven, sumOdd);
+	}
+
+	public static void main(String[] args) {
+		System.out.println(cashSum(cashPerHouse));
+	}
+}
