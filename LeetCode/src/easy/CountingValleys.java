1a370d08454c6774b664a604c7121c1cc754c371
Jamie Dishy <Jamie Dishy@192.168.0.14>
2019-10-26 23:53:18 -0400

    Day 14

---
+++ LeetCode/src/easy/CountingValleys.java
@@ -0,0 +1,28 @@
+package easy;
+
+public class CountingValleys {
+
+	public static int countValleys(int n, String s) {
+		int count = 0;
+		int height = 0;
+
+		for (int i = 0; i < n; i++) {
+			if (s.charAt(i) == 'D') {
+				height = height - 1;
+			} else {
+				height = height + 1;
+				if (height == 0) {
+					count++;
+				}
+			}
+		}
+
+		return count;
+	}
+
+	public static void main(String[] args) {
+		String path = "DDUUUUDD";
+		System.out.println(countValleys(8, path));
+	}
+
+}
