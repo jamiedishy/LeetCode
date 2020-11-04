ea4aad2c87102179415195e591fb26f2b1e15fbe
jamiedishy <jamiedishy@gmail.com>
2020-11-03 21:47:33 -0500

    Day 9 - mirror tree

--- LeetCode/src/easy/MirrorTree.java
+++ LeetCode/src/easy/MirrorTree.java
@@ -1,44 +1,40 @@
 package easy;
 
-
 public class MirrorTree {
-	
+
 	static class BinaryTree {
 		BinaryTree right;
 		BinaryTree left;
 		int data;
-		
+
 		public BinaryTree(int data) {
 			this.right = null;
 			this.left = null;
 			this.data = data;
 		}
 	}
-	
+
 	public static boolean isMirror(BinaryTree node) {
 		if (node.left != null && node.right != null) {
-		return mirrorHelper(node.left, node.right);
-		}
-		else if (node.left != null || node.right != null){ // unevenly balanced
+			return mirrorHelper(node.left, node.right);
+		} else if (node.left != null || node.right != null) { // unevenly balanced
 			return false;
 		}
 		return true; // if only one node
 	}
-	
+
 	public static boolean mirrorHelper(BinaryTree left, BinaryTree right) {
-		
+
 		if (left.left == null && left.right == null && right.left == null && right.right == null) {
 			if (left.data == right.data) {
 				return true;
-			} 
-			else {
-				return false;
 			}
-		}
-		else if (left.left == null || left.right == null || right.left == null || right.right == null) {
+			return false;
+		} else if (left.left == null || left.right == null || right.left == null || right.right == null) { // unevenly
+																											// balanced
 			return false;
 		}
-		return mirrorHelper(left.left, right.right) && mirrorHelper(left.right, right.left); 
+		return mirrorHelper(left.left, right.right) && mirrorHelper(left.right, right.left);
 	}
 
 	public static void main(String[] args) {
@@ -49,7 +45,7 @@ public class MirrorTree {
 		node.left.right = new BinaryTree(4);
 		node.right.left = new BinaryTree(4);
 		node.right.right = new BinaryTree(3);
-		
+
 		System.out.println(isMirror(node));
 	}
 
